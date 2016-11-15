0b945e529c42cbc91efcc2f85065719450bc7fa7
Vovas11 <Vovas11@users.noreply.github.com>
2016-11-15 11:52:49 +0100

    Add main controller JsonHandler.java

---
+++ src/main/java/com/devproserv/timetracking/controller/JsonHandler.java
@@ -0,0 +1,68 @@
+package com.devproserv.timetracking.controller;
+
+import com.devproserv.timetracking.command.Command;
+import com.devproserv.timetracking.command.CommandFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Main servlet handling user forms on HTML pages. Gets the link to the CommandFactory
+ * and depending on the command from the user form sent in HTTP request hands control
+ * over to the corresponding command.
+ * 
+ * @author vovas11
+ * @see CommandFactory
+ */
+@SuppressWarnings("serial")
+public class JsonHandler extends HttpServlet {
+    /**
+     * Selects a command corresponding to the parameter sent from the user form
+     * in HTTP request. Unites the methods doPost and doGet.
+     * 
+     * @param  request   HTTP request
+     * @param  response  HTTP response
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+
+	/* gets link to the CommandFactory instance or creates one if applies for the first time */
+	CommandFactory commandFactory = CommandFactory.getInstance();
+	
+	/* gets link to the command from the CommandFactory */
+	Command command = commandFactory.getCommand(request);
+	
+	/* runs the command, gets the page to display from the command and sends the page to the client */
+	String page = command.executeCommand(request);
+	RequestDispatcher reqDisp = request.getRequestDispatcher(page);
+	reqDisp.forward(request, response);
+    }
+    
+    /**
+     * Standard servlet method, 'overridden' by {@code processRequest} method
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+
+	/* call common method for POST and GET methods */
+	processRequest(request, response);
+    }
+    
+    /**
+     * Standard servlet method, 'overridden' by {@code processRequest} method
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+	
+	/* call common method for POST and GET methods */
+	processRequest(request, response);
+    }
+}
