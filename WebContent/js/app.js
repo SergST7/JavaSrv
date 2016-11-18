8a433e1746abb483143cfbc6efaf0a9a65bf0cd5
Vovas11 <Vovas11@users.noreply.github.com>
2016-11-18 23:45:55 +0100

    Add form for editing employee data

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,19 +1,44 @@
 var app = angular.module('myApp', [])
-	.controller('mainController', ['$scope', '$http', function($scope, $http) {
-		
-		$scope.getAllEmployees = function() {
-			$http({
-				method: 'GET',
-				url: 'jsonhandler',
-				params: {'command': 'allemployeesfull'}
-			}).then(function successCallback(response) {
-				$scope.status = response.status;
-				$scope.data = response.data;
-				$scope.employees = response.data;
-			}, function errorCallback(response) {
-				$scope.data = response.data || 'Request failed';
-				$scope.status = response.status;
-			});
-		};
-		
-	}]);
+.controller('MainController', ['$scope', '$http', function($scope, $http) {
+	
+	$scope.getAllEmployees = function() {
+		$http({
+			method: 'GET',
+			url: 'jsonhandler',
+			params: {'command': 'allemployeesfull'}
+		}).then(function successCallback(response) {
+			$scope.status = response.status;
+			$scope.data = response.data;
+			$scope.employees = response.data;
+		}, function errorCallback(response) {
+			$scope.data = response.data || 'Request failed';
+			$scope.status = response.status;
+		});
+	};
+	
+	$scope.editEmployee = function(id, idTime) {
+		$scope.toShow = true;
+		for (var i = 0; i < $scope.employees.length; i++) {
+			if ($scope.employees[i].idEmpl === id && $scope.employees[i].idTime === idTime) {
+				$scope.employeeToEdit = $scope.employees[i];
+			}
+		}
+	};
+	
+	$scope.employeeSave = function() {
+		$scope.toShow = false;
+		alert("to be developed");
+	};
+	
+	$scope.employeeDelete = function() {
+		$scope.toShow = false;
+		alert("to be developed");
+	};
+	
+	$scope.addNewEmployee = function() {
+		alert("to be developed");
+	};
+	
+	$scope.toShow = false;
+	
+}]);
